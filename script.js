7c23ad4a92397ef05df303132a003117b7970d0f
Blackbeardchris <christopherl10@learn.cssd.ab.ca>
2017-09-21 13:57:20 -0600

    IT WORKED
    FINALLY GOT THE TEXTAREA TO OUTPUT!

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var txtbox = document.getElementsByClassName("txtbox");
+var txtbox = document.getElementsByClassName("textarea");
 var output = document.querySelector(".output");
 
 var htmlCode = txtbox.value;
@@ -14,35 +14,12 @@ reset.addEventListener("click", function() {
 }
 
 
-txtbox.addEventListener("input", drawOutput);
+textarea.addEventListener("input", drawOutput);
 window.addEventListener("load", drawOutput);
-//HERE
-var htmlInput = document.querySelector(".html-input");
-var cssInput = document.querySelector(".css-input");
-var reset = document.getElementById("reset");
-var htmlCode = htmlInput.value;
-var cssCode = cssInput.value;
-var output = document.querySelector(".output");
 
-var styleElem = document.createElement('style');
-var headElem = document.querySelector('head');
-headElem.appendChild(styleElem);
-
-function drawOutput() {
-  output.innerHTML = htmlInput.value;
-  styleElem.textContent = cssInput.value;
+function output() {
+    alert("testing TextArea");
 }
-
-reset.addEventListener("click", function() {
-  htmlInput.value = htmlCode;
-  cssInput.value = cssCode;
-  drawOutput();
-});
-
-htmlInput.addEventListener("input", drawOutput);
-cssInput.addEventListener("input", drawOutput);
-window.addEventListener("load", drawOutput);
-
 
 
 
