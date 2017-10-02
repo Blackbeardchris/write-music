bd9e66aace4a2293dccc3c160d8079a7419fd286
Blackbeardchris <christopherl10@learn.cssd.ab.ca>
2017-10-02 13:43:32 -0600

    maybe

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-function music() {
+var txt = document.querySelector('textarea');
+var out = document.querySelector('p');
+txt.onkeyup = txt.onkeydown = function music() {
 var textAreaInput = document.getElementById("text").value;
     var output = document.getElementById("testingp");
     var result = document.getElementById('text').value.match( /[^.!?]+[.!?]+/g );
-    var words = textAreaInput.split(" ");    
+        if (!result) {
+        return;
+    }
+    var className = 'long';
+    var words = textAreaInput.split(" ");   
+    if (words < 3) {
+        className = "short";
+    } else if (words <= 5) {
+        className = "medium";
+    } else if (words < 20) {
+        className = "long";
+    }
+    //alert(result[0].split(" ").length);
+    out.innerHTML = txt.value;  
+};
 
-    alert(result[2].split(" ").length);  
-    
-}
-var txxt = document.querySelector('textarea');
-var out = document.querySelector('p');
 
-function updateOutput() {
-    out.innerHTML = txxt.value;
-
-}
-txxt.addEventListener('input', updateOutput);
-
+music();
+document.querySelector('textarea').on('keyup', music);
