2fe50ea823b350cf793a00b82e77ff9175773346
Blackbeardchris <31997901+Blackbeardchris@users.noreply.github.com>
2017-10-03 18:54:22 -0600

    finally
    It works but it counts the space after period as a word

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # write-music
+Overview:
+The code find and splits the values from the textarea using periods, exclamation marks and question marks.
+Line 3 is saying is activating everything when a key is pressed and released.
+Line 5 is the string that detects the amount of words
+Line 30-38 is explaining that when a certain amount of words are reached a font class is searched for in the css and it is combined with the most recent result.
+
+**IN ORDER TO WORK PROPERLY DON'T PUT SPACES AFTER PUNCTUATION(i cant seem to get it to work)**
+
+Planning:
+* first figure out how to output something from a txt area into a <p> tag
+
+Review: I think i was really hard, from this I learned to complexity of arrays and how creative you can get with using an array.
--- script.js
+++ script.js
@@ -1,115 +1,75 @@
-var txt = document.querySelector('textarea');
-var out = document.querySelector('p');
-txt.onkeyup = txt.onkeydown = function music() {
-var textAreaInput = document.getElementById("text").value;
-    //var output = document.getElementById("testingp");
-    var result = document.getElementById('text').value.match( /[^.!?]+[.!?]+/g );
-        if (!result) {
-        return;
-    }
-    var className = 'long';
-    //var words = textAreaInput.split(" ");
-    
-    var i = result.length;
-    //console.log(i);
-    var x = -1;
-    var numofwords = 0;
-    
-    
-    // Count number of words
-    while (x <= i) {
-        x++;
-        numofwords = result[x].split(" ").length
-        
-        if (x != 0){
-            numofwords--; // SUBTRACT 1
-        }
-    
-        // CHANGE FROM HERE
-        console.log(numofwords);
-        out.innerHTML = txt.value;
-    }
-    
-    
-    
-    
-    
-    
-    /*
-    if (words < 3) {
-        className = "short";
-    } else if (words <= 5) {
-        className = "medium";
-    } else if (words < 20) {
-        className = "long";
-    }
-   */
-    // console.log(result[0].split(" ").length);
-    //out.innerHTML = txt.value; 
-   /*
-    out.innerHTML = txt.value;  
-};
-
-/*
-var txt = document.querySelector('textarea');
+/*var txt = document.querySelector('textarea');
 var out = document.querySelector('p');
 txt.onkeyup = txt.onkeydown = function music() {
     var textAreaInput = document.getElementById("text").value;
-    //var output = document.getElementById("testingp");
+    //var output = document.getElementById("output");
     var result = document.getElementById('text').value.match( /[^.!?]+[.!?]+/g );
         if (!result) {
         return;
     }
-    var className = 'long';
-    // var words = textAreaInput.split(" "); 
-
-    
-    
-    
     // result is an array of sentences. 
     // however result[0] needs a space at the start for this to work.
-    
     // CODE TO ADD A SPACE IN result[0]
-    
-    
-    
     // How many sentences? (length of result array)
     var i = result.length;
     //console.log(i);
     var x = -1;
+    var body = document.body;
     var numofwords = 0;
     
-    
     // Count number of words
+
     while (x <= i) {
         x++;
         numofwords = result[x].split(" ").length
-        
-        if (x != 0){
+        if (x != 0) {
             numofwords--; // SUBTRACT 1
         }
-    
         // CHANGE FROM HERE
         console.log(numofwords);
-    
+        if (numofwords <= 2) {
+            out.innerHTML = "<font class='tiny'>" + result[x] + "</font color>";
+        } else if (numofwords <= 5) {
+            out.innerHTML = "<font class='short'>" +  result[x] + "</font color>";
+        } else if (numofwords <= 14) {
+            out.innerHTML = "<font class='medium'>" +  result[x] + "</font color>";
+        } else {
+            out.innerHTML = "<font class='long'>" +  result[x] + "</font color>";
+        }
     }
-    
-    
-    
-    
-    
-    
-    /*
-    if (words < 3) {
-        className = "short";
-    } else if (words <= 5) {
-        className = "medium";
-    } else if (words < 20) {
-        className = "long";
-    }
-   */
-    // console.log(result[0].split(" ").length);
-    //out.innerHTML = txt.value; 
-/*
-*/
+out.innerHTML = txt.value;
 };
+*/
+var input = document.querySelector('textarea')
+var result = document.querySelector('p')
+
+// When user types
+input.oninput = function () {
+  var sentences = input.value.match(/([^\.!\?]+[\.!\?]+)|([^\.!\?]+$)/g)
+  console.log(sentences)
+ 
+  // Clear contents of result
+  result.innerHTML = ' '
+ 
+     // Add colored sentences of input
+  for (var i = 0; i < sentences.length; i++) {
+      var sentence = sentences[i]
+    var length = sentence.split(' ').length
+    
+    var highlight = document.createElement('span')
+    highlight.textContent = sentence.trim()
+    
+    // Change the color
+        if (length >= 15) {
+        highlight.style.background = 'blue'
+      } else if (length >= 5) {
+        highlight.style.background = 'green'
+      } else if (length >= 3) {
+        highlight.style.background = 'red'
+    } else {
+        highlight.style.background = 'yellow'
+    }
+    
+    result.appendChild(highlight)
+  }
+}
